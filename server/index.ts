0f178b22de06287f60374a8a428c3725381a3253
AabhasAgarwal-AA <aabhasagarwal50@gmail.com>
2026-07-11 13:16:50 +0000

    prisma added

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import {createClient} from "redis";
+import { prisma } from "./db";
 
 const client = createClient({
     url: process.env.REDIS_URL,
@@ -11,15 +12,24 @@ console.log("working");
 const app = express();
 app.use(express.json());
 
-app.post("/submission", (req, res) => {
+app.post("/submission", async (req, res) => {
     const userId = req.body.userId; 
     const code = req.body.code;
     const language = req.body.language; 
 
-    client.lPush("problems", JSON.stringify({userId, code, language}));
+    const response = await prisma.submissions.create({
+        data: {
+            code,
+            language, 
+            status: "Processing"
+        }
+    });
+    
+    client.lPush("problems", JSON.stringify({submissionid: response.id, code, language}));
 
     res.json({
         message: "processing",
+        id: response.id
     })
 
 
